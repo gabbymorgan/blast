4d0f63af8350b7b653952c863420e2652808d257
Gabriella Morgan <gabriellapelton@gmail.com>
2024-02-17 17:13:15 -0600

    move persistent unique inputs to environment variables

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+import os
 import requests
 from fastapi import FastAPI
 from typing import Union
 from pydantic import BaseModel
 from atproto import Client, client_utils
 
-
 app = FastAPI()
 
 
-class StatusRequest(BaseModel):
-    mastodon_base_url: str
-    mastodon_token: str
-    bsky_username: str
-    bsky_password: str
+class SkeetRequest(BaseModel):
+    content: str
+
+
+class TootRequest(BaseModel):
     content: str
 
 
 @app.post("/skeet")
-def new_skeet(status_request: StatusRequest):
-    response = requests.post(f"https://{status_request.mastodon_base_url}/api/v1/statuses", data={
-                             'status': status_request.content}, headers={"Authorization": f"Bearer {status_request.mastodon_token}"})
+def new_skeet(skeet_request: SkeetRequest):
     client = Client()
-    profile = client.login(status_request.bsky_username,
-                           status_request.bsky_password)
-    post = client.send_post(status_request.content)
-    return "cool"
+    client.login(os.getenv('BSKY_USERNAME', 'Bluesky username not found'),
+                 os.getenv('BSKY_PASSWORD', 'Bluesky password not found'))
+    skeetResponse = client.send_post(skeet_request.content)
+    return skeetResponse
+
+
+@app.post("/toot")
+def new_skeet(status_request: TootRequest):
+    tootResponse = requests.post(f"https://{os.getenv('MASTODON_BASE_URL')}/api/v1/statuses", data={
+        'status': status_request.content}, headers={"Authorization": f"Bearer {os.getenv('MASTODON_TOKEN')}"})
+    return tootResponse
